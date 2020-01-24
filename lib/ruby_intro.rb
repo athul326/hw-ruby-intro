bb67f5dba7fcd98599a08562e8bb3dc9a032ba02
EC2 Default User <ec2-user@ip-172-31-93-182.ec2.internal>
2020-01-24 23:33:21 +0000

    final

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,100 @@
 
 def sum arr
   # YOUR CODE HERE
+  sumfinal = 0
+  for x in arr do
+    sumfinal = sumfinal + x
+  end
+  return sumfinal
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  sumfinal = 0
+  max1 = 0
+  max2 = 0
+  for x in arr do
+    if x >= max1
+      max2 = max1
+      max1 = x
+    elsif max2 == 0
+      max2 = x
+    end
+  end
+  sumfinal = max1+max2
+  return sumfinal
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  for x in arr do
+    for y in arr do
+      if y!=x and x+y == n
+        return true
+      end
+    end
+  end
+  return false
+  
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  vowels = ['a','e','i','o','u']
+  str = s.downcase
+  if s.length == 0
+    return false
+  end
+  if str[0] !~ /[[:alpha:]]/
+    return false
+  end
+  if vowels.include? str[0]
+    return false
+  end
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /[^01]/ or s.length ==0
+    return false
+  else
+    x = s.to_i(2)
+    if x%4 == 0
+      return true
+    end
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    raise ArgumentError.new("Price cannot be zero or negative") if price <= 0
+    raise ArgumentError.new("ISBN cannot be empty") if isbn.length == 0
+    @isbn = isbn
+    @price = price
+  end
+  def isbn
+    @isbn
+  end
+  def price
+    @price
+  end
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  def price=(price)
+    @price = price
+  end
+  def price_as_string
+    x = '%.2f' % @price
+    return "$"+x
+  end
 end
